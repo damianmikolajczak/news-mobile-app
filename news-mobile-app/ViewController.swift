eea1940c452cb11c3ac9bbeb99642d4c03dd3048
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-06-26 17:05:03 +0200

    Initial Commit

---
+++ news-mobile-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-mobile-app
+//
+//  Created by Damian Mikołajczak on 26/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
